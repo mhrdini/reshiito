ce831fadd207b24077b1019ed286e7c372542d43
Dini Putri <mahardini.rizky@gmail.com>
2025-09-06 20:45:23 +0900

    chore: pre-commit hooks to validate branch and commit names according to conventional config installed and configured commitizen and a convention adapter to dictate create names with pnpm "commit" script, and husky for pre-commit hooks that run commitlint, lint-staged, and validate-branch-name

--- config/prettier/index.js
+++ config/prettier/index.js
@@ -3,11 +3,16 @@ import { fileURLToPath } from 'url'
 /** @typedef {import("prettier").Config} PrettierConfig */
 /** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
 /** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
-
 /** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+
 const config = {
-  plugins: ['@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
-  tailwindConfig: fileURLToPath(new URL('../../configs/tailwind/index.ts', import.meta.url)),
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+  tailwindConfig: fileURLToPath(
+    new URL('../../config/tailwind/index.ts', import.meta.url),
+  ),
   tailwindFunctions: ['cn', 'cva'],
   importOrder: [
     '<TYPES>',
@@ -22,7 +27,7 @@ const config = {
     '<TYPES>^[.|..|~]',
     '^~/',
     '^[../]',
-    '^[./]'
+    '^[./]',
   ],
   importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
   importOrderTypeScriptVersion: '4.4.0',
@@ -33,7 +38,7 @@ const config = {
   printWidth: 80,
   arrowParens: 'avoid',
   singleQuote: true,
-  endOfLine: 'auto'
+  endOfLine: 'auto',
 }
 
 export default config
